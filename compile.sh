9811d40c68ec19ba1a14000c2360bb8fbd024026
Simone Colli <s.colli.dev@gmail.com>
2026-04-30 12:28:22 +0200

    compile.sh: forward extra args to g++

--- compile.sh
+++ compile.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# example use:
+# bash compile.sh <optional compiler flags>
+# bash compile.sh -DVERBOSE : enable verbose logging
+# bash compile.sh -DDEBUG
+# bash compile.sh -DVERBOSE -DDEBUG : enable both verbose logging and debug mode
 
-g++ create_hgt_pool.cpp -o create_hgt_pool
-g++ evolve.cpp -o evolve
+g++ create_hgt_pool.cpp -o create_hgt_pool "$@"
+g++ evolve.cpp -o evolve "$@"
