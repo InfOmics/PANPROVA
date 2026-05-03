40ae51f6eb3261e0bc842938e0633302d5ec97fc
Simone Colli <s.colli.dev@gmail.com>
2026-05-03 11:38:55 +0200

    utils: add comments

--- src/utils/randoms.hh
+++ src/utils/randoms.hh
@@ -8,16 +8,20 @@
 #include <algorithm>
 
 
+// random number generator
 std::mt19937_64 rng;
 std::uniform_real_distribution<double> unif(0, 1);
 
-
+// initialize the random seed for the random number generator
 void init_random_seed(int seed){
     std::srand ( unsigned ( seed ) );
     rng.seed(seed);
 }
 
+// returns a random integer in the range [0, i)
 int randint (int i) { return std::rand()%i;}
+
+// returns a random integer in the range [i, j)
 int randint (int i,int j) { return (std::rand()%(j-i))+i;}
 
 double generate_unif(){
